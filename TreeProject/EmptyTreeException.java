f88768ba919d936f110baddc88e1a10c65317193
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-03-10 22:53:43 -0800

    created public class EmptyTreeException extends RuntimeException
    created public EmptyTreeException()
    created public EmptyTreeException(String message)

--- TreeProject/EmptyTreeException.java
+++ TreeProject/EmptyTreeException.java
@@ -1,5 +1,15 @@
 package TreeProject;
 
-public class EmptyTreeException {
+public class EmptyTreeException extends RuntimeException 
+{
+    public EmptyTreeException() 
+    {
+        this(null);
+    } // end default constructor
     
-}
+    public EmptyTreeException(String message) 
+    {
+        super(message);
+    } // end constructor
+} // end EmptyTreeException
+
