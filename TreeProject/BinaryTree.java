c8c4eeab2debb48fbab1baca5ecfb1a21d8b0762
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-03-10 22:41:49 -0800

    created private void initializeTree(T rootData, BinaryTree<T> leftTree, BinaryTree<T> rightTree)

--- TreeProject/BinaryTree.java
+++ TreeProject/BinaryTree.java
@@ -57,4 +57,25 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
     {
         return root;
     } // end getRootNode
+
+    private void initializeTree(T rootData, BinaryTree<T> leftTree, BinaryTree<T> rightTree) 
+    {
+        root = new BinaryNode<>(rootData);
+        
+        if ((leftTree != null) && !leftTree.isEmpty())
+            root.setLeftChild(leftTree.root);
+        
+        if ((rightTree != null) && !rightTree.isEmpty()) {
+            if (rightTree != leftTree)
+                root.setRightChild(rightTree.root);
+            else
+                root.setRightChild(rightTree.root.copy());
+        } // end if
+        
+        if ((leftTree != null) && (leftTree != this))
+            leftTree.clear();
+        
+        if ((rightTree != null) && (rightTree != this))
+            rightTree.clear();
+    } // end initializeTree
 }
