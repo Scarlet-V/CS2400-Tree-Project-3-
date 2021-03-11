6b546ea20c387f80ed5d23a17cf455220f549972
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-03-10 22:29:22 -0800

    created public BinaryTree()

--- TreeProject/BinaryTree.java
+++ TreeProject/BinaryTree.java
@@ -1,5 +1,11 @@
 package TreeProject;
 
-public class BinaryTree {
-    
+public class BinaryTree<T> implements BinaryTreeInterface<T>  
+{
+    private BinaryNode<T> root;
+
+    public BinaryTree() 
+    {
+        root = null;
+    } // end default constructor
 }
