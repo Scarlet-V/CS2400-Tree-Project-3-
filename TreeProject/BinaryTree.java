0975d3393915cce127ca88363e52c75b07451d40
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-03-10 22:34:21 -0800

    created protected BinaryNode<T> getRootNode()

--- TreeProject/BinaryTree.java
+++ TreeProject/BinaryTree.java
@@ -52,4 +52,9 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
     {
         root = rootNode;
     } // end setRootNode
+
+    protected BinaryNode<T> getRootNode() 
+    {
+        return root;
+    } // end getRootNode
 }
