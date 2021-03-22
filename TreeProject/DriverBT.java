bd70ecd7a041a62c6a48f5b14663a89473c507e2
Scarlet-V <77875288+Scarlet-V@users.noreply.github.com>
2021-03-21 23:45:14 -0700

    create Tree2

--- TreeProject/DriverBT.java
+++ TreeProject/DriverBT.java
@@ -78,6 +78,28 @@ public class DriverBT
     public static void createTree2(BinaryTree<String> tree) 
     {
     
+     // Leaves
+     BinaryTree<String> bTree = new BinaryTree<>("B");
+     BinaryTree<String> fTree = new BinaryTree<>("F");
+     BinaryTree<String> gTree = new BinaryTree<>("G");
+     BinaryTree<String> hTree = new BinaryTree<>("H");
+     
+     // Subtrees
+     BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
+     BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
+     BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+     
+     tree.setTree("A", bTree, cTree);
+     
+     System.out.println("\nGiven Tree:\n");
+     System.out.println("     A      ");
+     System.out.println("   /   \\  ");
+     System.out.println("  B     C  ");
+     System.out.println("        / \\ ");
+     System.out.println("       D    E");
+     System.out.println("      /    /  \\");
+     System.out.println("     F    G     H");
+     System.out.println();
     } // end createTree2
     
 }
